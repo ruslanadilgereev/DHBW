8c3d384dc8e475191581215a86295459f1d74e5c
adilgereev <74386848+ruslanadilgereev@users.noreply.github.com>
2023-08-23 14:14:59 +0200

    Update GeneticAlgorithm.py
    Mit single point crossover und anschließender mutation

--- GeneticAlgorithm.py
+++ GeneticAlgorithm.py
@@ -2,42 +2,10 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 P_pcs_time = np.array([[139, 729, 382, 382, 468, 814],  ## Stückzahl der Produkte
-                       [79, 113, 79, 113, 46, 113],  ## Produktionszeit auf Band 1
-                       [43,  62, 43, 62, 12, 75],
-                       [141, 159, 141, 159, 62, 141],
-                       [145, 160, 145, 160, 80, 100]])  ## Produktionszeit auf Band 2
-
-
-##def fitness_calc1(P):
-####### Erste Variante
-##R = np.zeros((3,8))s
-##endzeit_B1 = np.zeros(8)
-##endzeit_B2 = np.zeros(8)
-##endzeit_B3 = np.zeros(8)
-
-##for n in range(8):
-##    # For Band 1
-##    if n == 0:
-##        endzeit_B1[n] = P[0,n]
-##    else:
-##        endzeit_B1[n] = endzeit_B1[n-1] + P[0,n]
-##    R[0,n] = 1  # Product is ready for Band 2
-##
-##    # For Band 2
-##    if n == 0:
-##        endzeit_B2[n] = endzeit_B1[n] + P[1,n]
-##    else:
-##        endzeit_B2[n] = max(endzeit_B1[n], endzeit_B2[n-1]) + P[1,n]
-##    R[1,n] = 1  # Product is ready for Band 3
-##
-##    # For Band 3
-##    if n == 0:
-##        endzeit_B3[n] = endzeit_B2[n] + P[2,n]
-##    else:
-##        endzeit_B3[n] = max(endzeit_B2[n], endzeit_B3[n-1]) + P[2,n]
-
-##gesamtzeit = endzeit_B3[-1]
-##print(gesamtzeit)
+                      [79,113, 78, 113, 46, 113],  ## Produktionszeit auf Band 1
+                      [43, 61, 42, 61,12,75], ## Produktionszeit auf Band 2
+                      [141,159,141,159,62,141], ## Produktionszeit auf Band 3
+                      [145,160,145,160,80,100]])  ## Produktionszeit auf Band 4)
 
 def fitness_calc2(P):
     """
@@ -91,43 +59,93 @@ def generate_individuum(P_pcs_time):
     return shuffled_array, shuffled_product_order
 
 
-def single_point_crossover(A, B):
-    A_new = np.zeros(A.shape, dtype=int)
-    B_new = np.zeros(B.shape, dtype=int)
-    x = np.random.randint(1, A.shape[1])
-    for i in range(A.shape[0]):
-        A_new[i] = np.append(A[i, :x], B[i, x:])
-        B_new[i] = np.append(B[i, :x], A[i, x:])
-    return A_new, B_new
+def single_point_crossover_1D(A, B, p):
+    if p <= 0:
+        p = np.random.randint(1,A.size)
+    x = np.random.randint(0, p)
+    child1 = np.concatenate((A[:x], B[x:]))
+    child2 = np.concatenate((B[:x], A[x:]))
+    return child1, child2
 
+def order_to_time(product_order):
+    array = []
+    n_band = P_pcs_time.shape[0] - 1
+
+    # Create the processing time representation based on the given product order
+    for j in range(n_band):
+        for product_index in product_order:
+            array.append(P_pcs_time[j + 1, product_index])
+
+    # Convert the array to a 2D matrix
+    time_matrix = np.array(array).reshape(-1, len(product_order))
+
+    return time_matrix
+
+def mutate(individuum, mutation_rate):
+    """Mutation_Count gibt die Anzahl der Mutation an die stattfinden soll.
+    Mutation_Rate gibt die Wahrscheinlichkeit an, dass eine Mutation stattfindet."""
+    if np.random.rand() < mutation_rate:
+        for i in range(mutation_count):
+            # Wähle zwei zufällige Indizes zum Austauschen
+            idx1, idx2 = np.random.randint(0, len(individuum), 2)
+            # Tausche die Elemente an diesen Indizes
+            individuum[idx1], individuum[idx2] = individuum[idx2], individuum[idx1]
+    return individuum
 
 if __name__ == "__main__":
-    generation = 0
-    pop_count = 10000
-    pop_fitness = {}
+    generation_count = 100  # Anzahl der Generationen
+    pop_count = 50          # Anzahl der Individuuen pro Population
+    mutation_rate = 0.3     # Wahrscheinlichkeit für eine Mutation
+    mutation_count = 5      # Anzahl der Mutationselemente
+
+    all_fit_values = []     # Liste, um die Fitnesswerte aller Individuen über alle Generationen zu speichern
     pop_order = {}
-    fit_value = []
-    ##while generation < 100:
+    pop_fitness = {}
+
+    plt.ion()
+    fig, ax = plt.subplots()
+    # Anpassungen an der X- und Y-Achse, Titel, Legende usw.
+    ax.set_xlabel('Individuum Nummer')
+    ax.set_ylabel('Fitness')
+    ax.set_title('Fitness über die Generationen')
+    ax.grid(True)
+    line, = ax.plot([])     # Leerer Plot, der später aktualisiert wird
+
     for i in range(pop_count):
-        ind_time, ind_code = generate_individuum(P_pcs_time)
+        ind_time, ind_order = generate_individuum(P_pcs_time)
         pop_fitness[i] = fitness_calc2(ind_time)
-        pop_order[i] = ind_code
+        pop_order[i] = ind_order
 
-    sorted_keys = sorted(pop_fitness, key=pop_fitness.get, reverse=True)
-    sorted_pop_fitness = {key: pop_fitness[key] for key in sorted_keys}
-    sorted_pop_order = {key: pop_order[key] for key in sorted_keys}
+    for generation in range(generation_count):
+        print(f"Generation: # {generation}")
+        # Sortieren aller Fitnesswerte dieser Population mit der Reihenfolge
+        sorted_keys = sorted(pop_fitness, key=pop_fitness.get)
+        sorted_pop_order = {key: pop_order[key] for key in sorted_keys}
+        top_individuals = [sorted_pop_order[key] for key in sorted_keys[:pop_count // 2]]
+        top_individuals_dict = {i: indiv for i, indiv in enumerate(top_individuals)}
 
-    print(sorted_pop_order[9999])
-    ##for i in range(pop_count):
-    ##    fit_value.append(sorted_pop_fitness[i][1])
-    ##print(fit_value)
+        k = len(top_individuals_dict)
+        while k < pop_count:
+            i, j = np.random.randint(0, len(top_individuals_dict), size=2)
+            child1, child2 = single_point_crossover_1D(top_individuals_dict[i],
+                                                       top_individuals_dict[j],
+                                                       np.random.randint(0, len(top_individuals_dict[0])-1))
+            top_individuals_dict[k] = mutate(child1, mutation_rate)
+            top_individuals_dict[k + 1] = mutate(child2, mutation_rate)
+            k += 2
 
+        for i in range(len(top_individuals_dict)):
+            pop_order[i] = top_individuals_dict[i]
+            current_fitness = fitness_calc2(order_to_time(top_individuals_dict[i]))
+            pop_fitness[i] = current_fitness
+            all_fit_values.append(current_fitness)      # Füge den aktuellen Fitnesswert direkt hinzu
+            all_fit_values.sort(reverse=True)           # Sortieren der Liste - absteigend
+            line.set_xdata(range(len(all_fit_values)))  # X-Daten aktualisieren
+            line.set_ydata(all_fit_values)              # Y-Daten aktualisieren
 
-    # Using seaborn to plot
-    plt.figure(figsize=(10, 5))
-    plt.plot(sorted_pop_fitness.values(), marker='o', linestyle='-', color='b')
-    plt.title("Float Values")
-    plt.xlabel("Index")
-    plt.ylabel("Value")
-    plt.grid(True)
+        ax.relim()  # Grenzen neu berechnen
+        ax.autoscale_view()  # Skaliert die Achse
+        plt.draw()  # Zeichnet den aktualisierten Plot
+        plt.pause(0.1)
+    plt.ioff()
     plt.show()
